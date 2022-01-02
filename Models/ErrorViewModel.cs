2b2d1c237d1c93a1c7ce24abde8bbcd3eb3df889
Ismael Restrepo Vélez <irestrepo@celerik.com>
2022-01-02 18:44:50 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpSimulator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
